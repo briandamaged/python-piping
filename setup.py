a8603f81b858d29c77cdf4468583f1161a184d71
Brian Lauber <constructible.truth@gmail.com>
2016-03-31 16:50:11 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'piping',
+    version = '0.1.0',
+    description = 'Python Object Pipes',
+    author = 'Brian Lauber',
+    author_email = 'constructible.truth@gmail.com',
+    packages = find_packages(exclude = ["tests"]),
+    install_requires = [],
+    test_suite = 'tests',
+    tests_require = ["mock>=1.0.0"]
+)
